b39eecbecf8aec5d033a1c4b921f2e05e6171282
Jake Urban <jake@stellar.org>
2021-04-27 09:43:00 -0700

    added prettier formatting

--- src/helpers/run.ts
+++ src/helpers/run.ts
@@ -2,10 +2,10 @@ import { Config, Result } from "../types";
 import { getSuites, runSuite } from "./suite";
 
 export async function* run(config: Config): AsyncGenerator<Result> {
-	const suites = getSuites(config);
-    for (const suite of suites) {
-        for await (const result of runSuite(suite, config)) {
-            yield result; 
-        }
+  const suites = getSuites(config);
+  for (const suite of suites) {
+    for await (const result of runSuite(suite, config)) {
+      yield result;
     }
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,63 +1,63 @@
 import { Request, Response } from "node-fetch";
 
 export type SEP = 1 | 6 | 10 | 12 | 24 | 31;
-export type SepConfig = { [key in SEP]: object }
+export type SepConfig = { [key in SEP]: object };
 export type OutputFormat = "coloredText" | "text" | "markdown" | "json";
 
 export interface Config {
-    homeDomain: string;
-    seps: SEP[];
-    verbose?: boolean;
-    currency?: string;
-    searchStrings?: [string];
-    sepConfig?: SepConfig,
-    mainnetMasterAccountSecret?: string;
-    outputFormat?: OutputFormat;
+  homeDomain: string;
+  seps: SEP[];
+  verbose?: boolean;
+  currency?: string;
+  searchStrings?: [string];
+  sepConfig?: SepConfig;
+  mainnetMasterAccountSecret?: string;
+  outputFormat?: OutputFormat;
 }
 
 export interface NetworkCall {
-    request: Request;
-    response?: Response;
+  request: Request;
+  response?: Response;
 }
 
 export interface Failure {
-    name: string,
-	text: string;
-    coloredText: string;
-    markdown: string;
+  name: string;
+  text: string;
+  coloredText: string;
+  markdown: string;
 }
 
 export interface Result {
-    test: Test; 
-    networkCalls: NetworkCall[];
-    suite?: Suite;
-    expected?: any;
-    actual?: any;
-    failure?: Failure;
+  test: Test;
+  networkCalls: NetworkCall[];
+  suite?: Suite;
+  expected?: any;
+  actual?: any;
+  failure?: Failure;
 }
 
 export interface Test {
-    assertion: string;
-    successMessage: string;
-    failureModes: Record<string, Failure>;
-    before?: () => Promise<void>;
-    run(config: Config): Promise<Result>;
-    after?: () => Promise<void>;
+  assertion: string;
+  successMessage: string;
+  failureModes: Record<string, Failure>;
+  before?: () => Promise<void>;
+  run(config: Config): Promise<Result>;
+  after?: () => Promise<void>;
 }
 
 export interface Suite {
-    name: string;
-    tests: Test[];
-    sep?: SEP;
-    before?: () => Promise<void>;
-    after?: () => Promise<void>;
+  name: string;
+  tests: Test[];
+  sep?: SEP;
+  before?: () => Promise<void>;
+  after?: () => Promise<void>;
 }
 
 export interface Stats {
-    total: Number;
-    passed: Number;
-    failed: Number;
-    sep?: SEP;
-    name?: string;
-    suiteStats?: [Stats];
+  total: Number;
+  passed: Number;
+  failed: Number;
+  sep?: SEP;
+  name?: string;
+  suiteStats?: [Stats];
 }
