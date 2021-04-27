7eca5632e5477e98a142132313551c1c44e0ea70
Jake Urban <jake@stellar.org>
2021-04-27 11:04:50 -0700

    Added husky

--- src/helpers/stats.ts
+++ src/helpers/stats.ts
@@ -1,11 +1,11 @@
 import { Result, Stats } from "../types";
-	
+
 // TODO
-export function getStats(results: Result[]): Stats { 
-	console.log(results);
-	return {
-		total: 1,
-		passed: 1,
-		failed: 0
-	} 
+export function getStats(results: Result[]): Stats {
+  console.log(results);
+  return {
+    total: 1,
+    passed: 1,
+    failed: 0,
+  };
 }
--- src/helpers/suite.ts
+++ src/helpers/suite.ts
@@ -1,65 +1,75 @@
 import { Suite, Config, Result } from "../types";
-import { suites as sep1Suites } from "../tests/sep1/tests"
+import { suites as sep1Suites } from "../tests/sep1/tests";
 
-export async function* runSuite(suite: Suite, config: Config): AsyncGenerator<Result> {
-	if (suite.before)
-		await suite.before();
-	for await (const test of suite.tests) {
-        if (test.before)
-    		await test.before();
-		const result = await test.run(config);
-        if (test.after)
-    		await test.after();
-		yield result;
-	}
-	if (suite.after) 
-		await suite.after();
+export async function* runSuite(
+  suite: Suite,
+  config: Config,
+): AsyncGenerator<Result> {
+  if (suite.before) {
+    await suite.before();
+  }
+  for await (const test of suite.tests) {
+    if (test.before) {
+      await test.before();
+    }
+    const result = await test.run(config);
+    if (test.after) {
+      await test.after();
+    }
+    yield result;
+  }
+  if (suite.after) {
+    await suite.after();
+  }
 }
 
-export function getSuites(config: Config): Suite[] { 
-	let suites: Suite[] = [];
-	if (config.seps.includes(1)) {
-		suites = suites.concat(sep1Suites);
-	}
-	return filterBySearchStrings(suites, config);
+export function getSuites(config: Config): Suite[] {
+  let suites: Suite[] = [];
+  if (config.seps.includes(1)) {
+    suites = suites.concat(sep1Suites);
+  }
+  return filterBySearchStrings(suites, config);
 }
 
 function filterBySearchStrings(suites: Suite[], config: Config): Suite[] {
-	if (!config.searchStrings) 
-		return suites;
-	const filteredSuites: Suite[] = [];
-	for (const suite of suites) {
-		let suiteMatched: boolean = false;
-		const suiteNameLower = suite.name.toLowerCase();
-		for (const searchString of config.searchStrings) {
-			const searchStringLower = searchString.toLowerCase();
-			if (suiteNameLower.includes(searchStringLower)) {
-				filteredSuites.push(suite)
-				suiteMatched = true;
-				break
-			}
-		}
-		if (suiteMatched) 
-			continue;
-		const filteredSuite: Suite = {
-			name: suite.name,
-			tests: []
-		};
-		if (suite.sep) filteredSuite.sep = suite.sep;
-		if (suite.before) filteredSuite.before = suite.before;
-		if (suite.after) filteredSuite.after = suite.after;
-		for (const test of suite.tests) {
-			const assertionLower = test.assertion.toLowerCase();
-			for (const searchString of config.searchStrings) {
-				const searchStringLower = searchString.toLowerCase();
-				if (assertionLower.includes(searchStringLower)) {
-					filteredSuite.tests.push(test);
-					break;
-				}
-			}
-		}
-		if (filteredSuite.tests)
-			filteredSuites.push(filteredSuite);
-	}
-	return filteredSuites;
+  if (!config.searchStrings) {
+    return suites;
+  }
+  const filteredSuites: Suite[] = [];
+  for (const suite of suites) {
+    let suiteMatched = false;
+    const suiteNameLower = suite.name.toLowerCase();
+    for (const searchString of config.searchStrings) {
+      const searchStringLower = searchString.toLowerCase();
+      if (suiteNameLower.includes(searchStringLower)) {
+        filteredSuites.push(suite);
+        suiteMatched = true;
+        break;
+      }
+    }
+    if (suiteMatched) {
+      continue;
+    }
+    const filteredSuite: Suite = {
+      name: suite.name,
+      tests: [],
+    };
+    if (suite.sep) filteredSuite.sep = suite.sep;
+    if (suite.before) filteredSuite.before = suite.before;
+    if (suite.after) filteredSuite.after = suite.after;
+    for (const test of suite.tests) {
+      const assertionLower = test.assertion.toLowerCase();
+      for (const searchString of config.searchStrings) {
+        const searchStringLower = searchString.toLowerCase();
+        if (assertionLower.includes(searchStringLower)) {
+          filteredSuite.tests.push(test);
+          break;
+        }
+      }
+    }
+    if (filteredSuite.tests) {
+      filteredSuites.push(filteredSuite);
+    }
+  }
+  return filteredSuites;
 }
