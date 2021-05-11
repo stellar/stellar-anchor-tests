1a65bf82c7c5efd36b7973f4fd002523793929bf
Jake Urban <jake@stellar.org>
2021-05-11 14:12:06 -0700

    added back index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export * from "./types";
+export { run, getTests, runTests } from "./helpers/test";
+export { default as sep1Tests } from "./tests/sep1/tests";
+export { default as sep10Tests } from "./tests/sep10/tests";
+export { default as sep24Tests } from "./tests/sep24/tests";
