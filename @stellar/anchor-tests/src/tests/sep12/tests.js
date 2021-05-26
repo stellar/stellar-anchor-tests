c6e73b4c76c3cc5b4f0a57466c4391de371346f1
Piyal Basu <pbasu235@gmail.com>
2021-05-26 17:09:42 -0400

    rm js files

--- @stellar/anchor-tests/src/tests/sep12/tests.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var getCustomer_1 = require("./getCustomer");
-var putCustomer_1 = require("./putCustomer");
-var deleteCustomer_1 = require("./deleteCustomer");
-exports["default"] = putCustomer_1["default"].concat(getCustomer_1["default"], deleteCustomer_1["default"]);
--- @stellar/anchor-tests/src/tests/sep24/tests.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var toml_1 = require("./toml");
-var info_1 = require("./info");
-var deposit_1 = require("./deposit");
-var withdraw_1 = require("./withdraw");
-var transaction_1 = require("./transaction");
-var transactions_1 = require("./transactions");
-exports["default"] = toml_1["default"].concat(info_1["default"], deposit_1["default"], withdraw_1["default"], transaction_1["default"], transactions_1["default"]);
