ab812be4e9650baf161e45f9703c76ebd33cfa5b
Jake Urban <jake@stellar.org>
2021-06-15 08:30:21 -0700

    test that env var is present

--- ui/src/helpers/socketConnection.ts
+++ ui/src/helpers/socketConnection.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
 export const socket = io(
-  process.env.REACT_APP_SERVER_HOST || "http://localhost:8000",
+  process.env.REACT_APP_SERVER_HOST || "https://localhost:8000",
   { path: "/api" }
 );
