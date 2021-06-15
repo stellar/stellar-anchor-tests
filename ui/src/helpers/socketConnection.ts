3a5f00fa66243e1d140412c8748445304f9fce08
Jake Urban <jake@stellar.org>
2021-06-14 21:59:13 -0700

    use REACT_APP_ prefix for UI environment variables =

--- ui/src/helpers/socketConnection.ts
+++ ui/src/helpers/socketConnection.ts
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io(process.env.SERVER_HOST || "localhost:8000", {
-  path: "/api",
-});
+export const socket = io(
+  process.env.REACT_APP_SERVER_HOST || "http://localhost:8000",
+  { path: "/api" }
+);
