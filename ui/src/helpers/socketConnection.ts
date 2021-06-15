9b6a7e4755b32f5c67be35af7ce7c755699d2aa1
Jake Urban <jake@stellar.org>
2021-06-14 20:18:01 -0700

    add SERVER_HOST run-time environment variable to docker image

--- ui/src/helpers/socketConnection.ts
+++ ui/src/helpers/socketConnection.ts
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-export const socket = io(process.env.SERVER_HOST || "http://localhost:8000", {
+export const socket = io(process.env.SERVER_HOST || "localhost:8000", {
   path: "/api",
 });
