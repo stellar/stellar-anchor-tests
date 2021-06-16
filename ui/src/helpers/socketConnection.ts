8d7bc67fea0f26add041118de5ebe3f66e1d88f0
Jake Urban <jake@stellar.org>
2021-06-16 13:39:44 -0700

    make change for pr build

--- ui/src/helpers/socketConnection.ts
+++ ui/src/helpers/socketConnection.ts
@@ -1,7 +1,6 @@
 import io from "socket.io-client";
 
 console.log(process.env.REACT_APP_SERVER_HOST);
-console.dir(process.env);
 
 export const socket = io(
   process.env.REACT_APP_SERVER_HOST || "http://localhost:8000",
