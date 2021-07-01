93dd3f5292b14effbc2d3260e34fc7552412cc0e
Piyal Basu <pbasu235@gmail.com>
2021-07-01 15:32:15 -0400

    add resource links to UI

--- ui/src/types/testCases.ts
+++ ui/src/types/testCases.ts
@@ -1,3 +1,5 @@
+import { Response, Request } from "node-fetch";
+
 export interface FormData {
   homeDomain: string;
   seps: Array<number>;
@@ -12,9 +14,26 @@ export enum RunState {
   done = "done",
 }
 
+export interface NetworkCall {
+  request: Request & { headers: { [key: string]: string[] } };
+  response: Response;
+}
+
+export interface Result {
+  actual: string;
+  expected: string;
+  failureMessage: string;
+  failureMode: string;
+  networkCalls: NetworkCall[];
+  resourceLinks: {
+    [key: string]: string;
+  };
+  skipped: boolean;
+}
+
 export interface TestCase {
   test: any; // Test from @stellar/anchor-tests
-  result?: any; // Result from @stellar/anchor-tests
+  result?: Result; // Result from @stellar/anchor-tests
 }
 
 export interface TestCaseProgress {
