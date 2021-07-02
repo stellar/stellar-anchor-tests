0c29acccc3b64321797fac757cc2e2bcb9b0c860
Piyal Basu <pbasu235@gmail.com>
2021-07-01 20:17:43 -0400

    add sentry to UI (#43)

--- ui/src/views/App.tsx
+++ ui/src/views/App.tsx
@@ -1,9 +1,20 @@
 import { useEffect } from "react";
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 import { Layout } from "@stellar/design-system";
 
 import { METRIC_NAMES } from "constants/metricNames";
 import { emitMetric } from "helpers/metrics";
 import { TestRunner } from "components/TestRunner";
+
+if (process.env.REACT_APP_SENTRY_KEY) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_KEY,
+    release: `anchor-validator@${process.env.npm_package_version}`,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 export const App = () => {
   useEffect(() => {
@@ -17,7 +28,6 @@ export const App = () => {
       >
         <p>A test suite for validating SEP6, SEP24, SEP31 transfer servers.</p>
       </Layout.Header>
-
       <Layout.Content>
         <Layout.Inset>
           <TestRunner />
