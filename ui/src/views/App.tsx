3580524f5a61242491a2635feb5b7110a9c80a45
Piyal Basu <pbasu235@gmail.com>
2021-07-01 20:13:53 -0400

    add sentry to UI

--- ui/src/views/App.tsx
+++ ui/src/views/App.tsx
@@ -1,6 +1,17 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 import { Layout } from "@stellar/design-system";
 
 import { TestRunner } from "components/TestRunner";
+
+if (process.env.REACT_APP_SENTRY_KEY) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_KEY,
+    release: `anchor-validator@${process.env.npm_package_version}`,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 export const App = () => (
   <>
