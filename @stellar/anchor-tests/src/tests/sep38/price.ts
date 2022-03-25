4149129bab6be8186b54247f40c558ed720be881
Marcelo Salloum <marcelo@stellar.org>
2022-03-25 14:22:59 -0300

    Stopped requiring SEP-10 authentication for `GET /price` and `GET /prices` requests, to be compliant with the change made in https://github.com/stellar/stellar-protocol/pull/1144.

--- @stellar/anchor-tests/src/tests/sep38/price.ts
+++ @stellar/anchor-tests/src/tests/sep38/price.ts
@@ -9,60 +9,6 @@ import { makeRequest } from "../../helpers/request";
 import { priceSchema } from "../../schemas/sep38";
 import { returnsValidJwt } from "../sep10/tests";
 
-export const requiresJwt: Test = {
-  sep: 38,
-  assertion: "requires SEP-10 authentication",
-  group: "GET /price",
-  dependencies: [hasQuoteServer],
-  context: {
-    expects: {
-      quoteServerUrl: undefined,
-      sep38StellarAsset: undefined,
-    },
-    provides: {},
-  },
-  failureModes: {
-    INVALID_ERROR_SCHEMA: {
-      name: "invalid error schema",
-      text(_args: any): string {
-        return "All error responses should contain a JSON body with an 'error' key-value pair";
-      },
-      links: {
-        "SEP-38 Errors":
-          "https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0038.md#errors",
-      },
-    },
-    ...genericFailures,
-  },
-  async run(_config: Config): Promise<Result> {
-    const result: Result = { networkCalls: [] };
-    const networkCall: NetworkCall = {
-      request: new Request(
-        this.context.expects.quoteServerUrl +
-          "/price?" +
-          new URLSearchParams({
-            sell_asset: this.context.provides.sep38StellarAsset,
-            buy_asset: this.context.provides.sep38OffChainAsset,
-            sell_amount: "100",
-          }),
-      ),
-    };
-    result.networkCalls.push(networkCall);
-    const errorJSON = await makeRequest(
-      networkCall,
-      403,
-      result,
-      "application/json",
-    );
-    if (!errorJSON) return result;
-    if (!errorJSON.error || typeof errorJSON.error !== "string") {
-      result.failure = makeFailure(this.failureModes.INVALID_ERROR_SCHEMA);
-      return result;
-    }
-    return result;
-  },
-};
-
 /*
  * Its possible that the anchor only supports converting off-chain
  * assets to on-chain assets (deposit-only), in which case this
@@ -370,7 +316,6 @@ export const deliveryMethodIsOptional: Test = {
 };
 
 export default [
-  requiresJwt,
   returnsValidResponse,
   amountsAreValid,
   acceptsBuyAmounts,
