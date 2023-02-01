3aa638d70ccfb79d9c9337ff727c388d14737c40
juansolana <jsolanacasas@gmail.com>
2023-02-01 15:07:12 -0600

    Updated sep6 config file to fix test issues

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yarn stellar-anchor-tests --seps 1 --sep-config ./@stellar/anchor-tests/src/tests/config/sep1.json --home-domain api-stage.anclap.ar
-yarn stellar-anchor-tests --seps 6 --sep-config ./@stellar/anchor-tests/src/tests/config/sep6.json --home-domain api-stage.anclap.ar
+yarn stellar-anchor-tests --seps 6 --asset-code USDC --sep-config ./@stellar/anchor-tests/src/tests/config/sep6.json --home-domain api-stage.anclap.ar
 yarn stellar-anchor-tests --seps 10 --sep-config ./@stellar/anchor-tests/src/tests/config/sep10.json --home-domain api-stage.anclap.ar
 yarn stellar-anchor-tests --seps 12 --sep-config ./@stellar/anchor-tests/src/tests/config/sep12.json --home-domain api-stage.anclap.ar
 yarn stellar-anchor-tests --seps 24 --sep-config ./@stellar/anchor-tests/src/tests/config/sep24.json --home-domain api-stage.anclap.ar
