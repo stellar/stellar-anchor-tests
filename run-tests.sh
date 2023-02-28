21c91d22e3b17273f6fbfbdf8a9f85c6b07582f2
juansolana <jsolanacasas@gmail.com>
2023-02-28 16:02:46 -0600

    updated to run sep 381

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,6 @@ yarn stellar-anchor-tests --seps 12 --sep-config ./@stellar/anchor-tests/src/tes
 yarn stellar-anchor-tests --seps 24 --sep-config ./@stellar/anchor-tests/src/tests/config/sep24.json --home-domain api-stage.anclap.ar
 yarn stellar-anchor-tests --seps 31 --asset-code USDC --sep-config ./@stellar/anchor-tests/src/tests/config/sep31.json --home-domain api-stage.anclap.ar
 yarn stellar-anchor-tests --seps 31 --asset-code USDC --sep-config ./@stellar/anchor-tests/src/tests/config/sep31and38.json --home-domain api-stage.anclap.ar
-yarn stellar-anchor-tests --seps 38 --sep-config ./@stellar/anchor-tests/src/tests/config/sep38.json --home-domain api-stage.anclap.ar
+yarn stellar-anchor-tests --seps 381 --sep-config ./@stellar/anchor-tests/src/tests/config/sep381.json --home-domain api-stage.anclap.ar
 
 
