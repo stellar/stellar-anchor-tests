2979ed6784291840b6f6cb347244ca1d8c5c42ee
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2023-07-05 12:04:03 -0300

    Move schemas around

--- @stellar/anchor-tests/src/schemas/sep24.ts
+++ @stellar/anchor-tests/src/schemas/sep24.ts
@@ -1,58 +1,4 @@
-// =========================== > TRANSACTION schema =========================== //
-const depositAndWithdrawInfoSchema = {
-  type: "object",
-  patternProperties: {
-    ".*": {
-      properties: {
-        enabled: { type: "boolean" },
-        fee_fixed: { type: "number" },
-        fee_minimum: { type: "number" },
-        fee_percent: { type: "number" },
-        min_amount: { type: "number" },
-        max_amount: { type: "number" },
-      },
-      required: ["enabled"],
-      additionalProperties: false,
-    },
-  },
-};
-
-export const infoSchema = {
-  type: "object",
-  properties: {
-    deposit: depositAndWithdrawInfoSchema,
-    withdraw: depositAndWithdrawInfoSchema,
-    fee: {
-      type: "object",
-      properties: {
-        enabled: { type: "boolean" },
-        authentication_required: { type: "boolean" },
-      },
-      required: ["enabled"],
-    },
-  },
-  required: ["deposit", "withdraw", "fee"],
-};
-
-export const successResponseSchema = {
-  type: "object",
-  properties: {
-    type: {
-      type: "string",
-      pattern: "interactive_customer_info_needed",
-    },
-    url: {
-      type: "string",
-      format: "uri",
-    },
-    id: {
-      type: "string",
-    },
-  },
-  required: ["type", "url", "id"],
-  additionalProperties: false,
-};
-
+// =========================== > TRANSACTION schema < =========================== //
 const transactionSchema = {
   type: "object",
   properties: {
@@ -278,7 +224,63 @@ export function getTransactionSchema(
   return schema;
 }
 
-// =========================== > CONFIG FILE schema =========================== //
+// ============================= > GET INFO schema < ============================ //
+const depositAndWithdrawInfoSchema = {
+  type: "object",
+  patternProperties: {
+    ".*": {
+      properties: {
+        enabled: { type: "boolean" },
+        fee_fixed: { type: "number" },
+        fee_minimum: { type: "number" },
+        fee_percent: { type: "number" },
+        min_amount: { type: "number" },
+        max_amount: { type: "number" },
+      },
+      required: ["enabled"],
+      additionalProperties: false,
+    },
+  },
+};
+
+export const infoSchema = {
+  type: "object",
+  properties: {
+    deposit: depositAndWithdrawInfoSchema,
+    withdraw: depositAndWithdrawInfoSchema,
+    fee: {
+      type: "object",
+      properties: {
+        enabled: { type: "boolean" },
+        authentication_required: { type: "boolean" },
+      },
+      required: ["enabled"],
+    },
+  },
+  required: ["deposit", "withdraw", "fee"],
+};
+
+// ======================= > INTERACTIVE RESPONSE schema < ====================== //
+export const successResponseSchema = {
+  type: "object",
+  properties: {
+    type: {
+      type: "string",
+      pattern: "interactive_customer_info_needed",
+    },
+    url: {
+      type: "string",
+      format: "uri",
+    },
+    id: {
+      type: "string",
+    },
+  },
+  required: ["type", "url", "id"],
+  additionalProperties: false,
+};
+
+// =========================== > CONFIG FILE schema < =========================== //
 const initialConfigSchema = {
   type: "object",
   properties: {
